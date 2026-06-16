2067529d2113e0017bd3482325d094603ea67485
swathikomeravelli <komswathi@gmail.com>
2026-06-16 16:01:54 +0100

    SF UTAM BDD JS

--- pageObjects/account.creation.page.js
+++ pageObjects/account.creation.page.js
@@ -3,15 +3,11 @@
 dotenv.config();*/
 //import * as Constants from "../utils/userDetails.js";
 
-import {error} from "@salesforce/sfdx-lwc-jest/src/log";
-let baseUrl = process.env.BASE_URL;
-let Page = require('./page.js');
 let BasePage = require('./base.page.js');
 const SESSION_TIMEOUT = 2 * 60 * 60 * 1000;
 
 let listViewHeader;
 let listViewName;
-
 const dropdownFields = new Set(['RATING', 'TYPE', 'OWNERSHIP', 'INDUSTRY', 'BILLING_COUNTRY', 'SHIPPING_COUNTRY', 'CUSTOMER_PRIORITY']);
 const baseInputFields = new Set(['ACCOUNT_NAME', 'ACCOUNT_NUMBER', 'ACCOUNT_SITE', 'TICKER_SYMBOL', 'ANNUAL_REVENUE', 'SIC_CODE']);
 const inputFields = new Set(['PHONE', 'FAX', 'WEBSITE', 'EMPLOYEES', 'SLA_SERIAL_NUMBER', 'BILLING_CITY', 'SHIPPING_CITY', 'BILLING_ZIP/POSTAL_CODE', 'SHIPPING_ZIP/POSTAL_CODE']);
@@ -57,39 +53,42 @@ const {
 } = require('../pageObjects/index.js');
 class AccountCreation extends BasePage {
 
-
-    async  chooseDropdown(item, value) {
-        console.log(await item);
-        const dropDownField = await item.getPicklist();
-        const baseComboBoxField = await dropDownField.getBaseCombobox();
-        await baseComboBoxField.expand();
-        const items = await baseComboBoxField.getItems();
-        for (const item of items) {
-            const itemLabel = await item.getItemLabel();
-            if (itemLabel === value) {
-                await item.clickItem();
-                break;
-            }
-        }
+    async verifyAccountsSortedRecent() {
+        await browser.waitUntil(
+            async () => {
+                try {
+                    const lv = await utam.load(ObjectHome);
+                    const lvm = await lv.getListViewManager();
+                    const cl = await lvm.getCommonListInternal();
+                    listViewHeader = await cl.getHeader();
+                    listViewName = await listViewHeader.getListViewTitleViaPicker();
+                    return listViewName && listViewName.length > 0;
+                } catch {
+                    return false;
+                }
+            },
+            { timeout: 15000, interval: 500, timeoutMsg: 'List view title did not load within 15s' }
+        );
+        expect(listViewName).toEqual('Recently Viewed');
     }
 
-    async  enterBaseInputField(item, value) {
-        const baseInput = await item.getLightningInputWrapper();
-        const lightningInput = await baseInput.getInput();
-        await lightningInput.setText(value);
+    async getAppContextName() {
+        const appNavAfterNav = await (await utam.load(DesktopLayoutContainer)).getAppNav();
+        const appName = await (await appNavAfterNav.getAppName()).getText();
+        return appName
     }
 
-    async  enterInputField(item, value) {
-        const lightningInput = await item.getInput();
-        await lightningInput.setText(value);
+    async createAccount() {
+        const actionsContainer = await listViewHeader.getAuraActionsContainer();
+        await (await actionsContainer.getActionLink('New')).click();
+        const modal = await utam.load(RecordActionWrapper);
+        const recordForm = await modal.getRecordForm();
+        const recordLayout = await recordForm.getRecordLayout();
+        await recordLayout.waitForSections();
+        await this.fillFormFields(recordLayout, fieldData);
     }
 
-    async  enterTextField(item, value) {
-        const lightningInput = await item.getInput();
-        await lightningInput.setText(value);
-    }
-
-    async  fillFormFields(recordLayout, fieldData) {
+    async fillFormFields(recordLayout, fieldData) {
         const sections = await recordLayout.getSections();
         for (const section of sections) {
             const rows = await section.getRows();
@@ -102,7 +101,6 @@ class AccountCreation extends BasePage {
                         fieldName = label.replaceAll(' ', '_').toUpperCase();
                     }
                     const value = fieldData[label];
-
                     if (!value)
                         continue;
                     if (dropdownFields.has(fieldName)) {
@@ -119,42 +117,35 @@ class AccountCreation extends BasePage {
         }
     }
 
-    async createAccount() {
-        const actionsContainer = await listViewHeader.getAuraActionsContainer();
-        await (await actionsContainer.getActionLink('New')).click();
-        const modal = await utam.load(RecordActionWrapper);
-        const recordForm = await modal.getRecordForm();
-        const recordLayout = await recordForm.getRecordLayout();
-        await recordLayout.waitForSections();
-        await this.fillFormFields(recordLayout, fieldData);
+    async chooseDropdown(item, value) {
+        console.log(await item);
+        const dropDownField = await item.getPicklist();
+        const baseComboBoxField = await dropDownField.getBaseCombobox();
+        await baseComboBoxField.expand();
+        const items = await baseComboBoxField.getItems();
+        for (const item of items) {
+            const itemLabel = await item.getItemLabel();
+            if (itemLabel === value) {
+                await item.clickItem();
+                break;
+            }
+        }
     }
 
-    async verifyAccountsSortedRecent() {
-
-        await browser.waitUntil(
-            async () => {
-                try {
-                    const lv = await utam.load(ObjectHome);
-                    const lvm = await lv.getListViewManager();
-                    const cl = await lvm.getCommonListInternal();
-                    listViewHeader = await cl.getHeader();
-                    listViewName = await listViewHeader.getListViewTitleViaPicker();
-                    return listViewName && listViewName.length > 0;
-                } catch {
-                    return false;
-                }
-            },
-            { timeout: 15000, interval: 500, timeoutMsg: 'List view title did not load within 15s' }
-        );
-
-        // assert that you have selected the correct list view
-        expect(listViewName).toEqual('Recently Viewed');
+    async enterBaseInputField(item, value) {
+        const baseInput = await item.getLightningInputWrapper();
+        const lightningInput = await baseInput.getInput();
+        await lightningInput.setText(value);
     }
 
-    async getAppContextName() {
-        const appNavAfterNav = await (await utam.load(DesktopLayoutContainer)).getAppNav();
-        const appName = await (await appNavAfterNav.getAppName()).getText();
-        return appName
+    async enterInputField(item, value) {
+        const lightningInput = await item.getInput();
+        await lightningInput.setText(value);
+    }
+
+    async  enterTextField(item, value) {
+        const lightningInput = await item.getInput();
+        await lightningInput.setText(value);
     }
 }
 module.exports = new AccountCreation();
